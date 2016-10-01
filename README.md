bf7c496de34c08fe2cd36f2c8ed380a656f313c0
Fred-Barclay <Fred-Barclay@users.noreply.github.com>
2016-10-01 01:19:40 -0500

    Initial commit (still need translations)

---
+++ README.md
@@ -0,0 +1,87 @@
+Caja Wipe
+===============
+
+An extension for Caja to securely delete files , using the `secure-delete`
+programme.
+
+Description
+==============
+
+Caja Wipe is a small Python 3 program for the Caja file manager (the default
+file manager in MATE) that adds options to the right-click menu to securely
+delete files.
+`srm` is called when deleting files. By default, `srm` uses:  
+```
+*      1 pass with 0xff
+
+*      5 random passes. /dev/urandom is used for a secure RNG if available.
+
+*      27 passes with special values defined by Peter Gutmann.
+
+*      5 random passes. /dev/urandom is used for a secure RNG if available.
+
+*      Rename the file to a random value
+
+*      Truncate the file
+
+As an additional measure of security, the file is opened in O_SYNC mode and
+after each pass an fsync() call is done.  srm writes 32k blocks for the
+purpose of speed, filling buffers of disk caches to force them to flush and
+overwriting old data which belonged to the file.
+```
+
+Examples
+==============
+You want to completely remove a document with your bank
+account number from your computer.
+
+Building and Installing
+=======================
+Debian (and Debian-based distros)  
+a) Download source code  
+```
+git clone https://github.com/Fred-Barclay/Caja-Wipe.git
+cd Caja-Wipe.git
+dpkg-buildpackage
+cd .. && sudo dpkg -i install caja-wipe_0.8.9_all.deb
+```
+
+
+Limitations
+==============
+(Adapted from the man pages of `srm`):
+ - NFS:    Beware of NFS. You can't ensure you really completely wiped your data
+from the remote disks, especially because of caching.
+
+ - Raid:   Raid Systems use stripped disks and have got large caches. It's hard
+wipe them.
+
+ - Swap: For secure deletion of the swap space, use `sswap`. Due to the great
+difficulty in setting up a generic implementation to `sswap`, and the potential
+for system damage this command might cause, Caja Wipe does not have a option
+to this so you will have to use the command line.
+
+ - Some of your data might have a temporary (deleted) copy somewhere on the
+disk.
+
+	If this is a concern, the secure-delete suite, which should be installed as
+a dependency to Caja Wipe on Debian-based distros (Debian, Linux Mint, etc),
+comes with the `sfill` command which can be used to delete *all* free space on
+your hard drive. Beware that this may take a very long time. If you abort the
+process before it ends, you may end up with extra files in various locations
+that may take up a great deal of space.
+
+Credits
+==============
+Portions of this README were copied and modified from the man page to `srm`;
+copyright van Hauser / THC <vh@thc.org>.
+
+License
+==============
+Caja Wipe is dual-licensed under the GPLv2, or, at your option, any later
+version; and custom, highly-permissive licensing terms.
+
+The text for both can be found in the COPYING file. A list of authors is in the
+AUTHORS file.
+
+Copyright (C) 2016 Caja Wipe authors.
---
+++ src/caja-wipe.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Caja Wipe - Extension for Caja to wipe files and/or free disk space.
+# Copyright (C) 2016 Caja Wipe authors.
+# Dual-licensed under the GPLv2 (or, at your option, any later version) and
+# custom licensing terms:
+# GPL v2:
+#   This program is free software; you can redistribute it and/or
+#   modify it under the terms of the GNU General Public License
+#   as published by the Free Software Foundation; either version 2
+#   of the License, or (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program; if not, write to the Free Software
+#   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+# Custom license terms:
+#   You are hereby granted a perpetual, irrevocable license to copy, modify,
+#   publish, release, and distribute this program as you see fit. However,
+#   under no circumstances may you claim original authorship of this program;
+#   you must credit the original author(s). You may not remove the listing of
+#   the original author(s) from the source code, though you may change the
+#   licensing terms. If you publish or release binaries or similarly compiled
+#   files, you must credit the author(s) on your home and/or distribution page,
+#   whichever applies. In your documentation, you must credit the author(s) for
+#   the portions of their code you have used. This, of course, does not revoke
+#   or change your right to claim original authorship to any portions of the
+#   code that you have written.
+#
+#   You must agree to assume all liability for your use of the program, and to
+#   indemnify and hold harmless the author(s) of this program from any liability
+#   arising from use of this program, including, but not limited to: loss of
+#   data, death, dismemberment, or injury, and all consequential and
+#   inconsequential damages.
+#
+#   For clarification, contact Fred Barclay:
+#       https://github.com/Fred-Barclay
+#       BugsAteFred@gmail.com
+
+import os
+from gi.repository import Caja, GObject
+from gettext import ngettext
+
+class CajaWipe(GObject.GObject, Caja.MenuProvider):
+	'''Simple Caja extension to wipe files, using the secure-delete suite.'''
+	def __init__(self):
+		pass
+	# If we are securely deleting a file/directory...
+	def get_file_items(self, window, files):
+		"""Returns the menu items to display when one or more files/folders are
+		selected."""
+
+		# I need this in wipe_file
+		global filelist
+		global dir_path
+
+		dir_path = None # is this necessary?
+		filelist = []
+		for file in files:
+			if dir_path == None: # first file: find path to directory
+				dir_path = file.get_parent_location().get_path()
+			name = file.get_name()
+			filelist += [name]
+
+		if dir_path == None or len(filelist) == 0:
+			return
+
+		# Make sure that the user has write permissions in this directory
+		if not os.access(dir_path, os.W_OK):
+			return
+
+		item = Caja.MenuItem(
+			name='SimpleMenuExtension::Wipe_File',
+			label=ngettext('Wipe File', 'Wipe Files', len(files)),
+			tip=ngettext('Wipe File', 'Wipe Files', len(files))
+		)
+		item.connect('activate', self.wipe_file)
+		return [item]
+
+	# Aaaannnnnndddddd....... ACTION!
+	def wipe_file(self, menu):
+		for filename in filelist:
+			path = dir_path+"/"+filename
+			print(filelist) # Debugging
+			print(filename) # Debugging
+			print(path) # Debugging
+			cmd = "srm -rv "+path # -v is good for debugging
+			os.system(cmd)
